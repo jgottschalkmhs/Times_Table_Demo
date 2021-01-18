df732fe5da4907f0933558507812e6d5809fd8c9
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2021-01-18 14:24:35 +1300

    fixed loop
    (goes from 1 to requested number)

--- 01_basic_times.py
+++ 01_basic_times.py
@@ -5,7 +5,7 @@ table = int(input("Which times table? "))
 how_high = int(input("How high? "))
 
 # output via a loop
-for item in range(0, how_high):
+for item in range(1, how_high + 1):
     times_table = "{} × {} = {}"\
         .format(item, table, item * table)
     print(times_table)
